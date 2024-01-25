ad89889a22a64bb4eda7e0ee8c5a5f5810536ad7
Soma <elena.valieva.QA@gmail.com>
2024-01-25 21:45:24 +0400

    ADD: test folder
    ADD: test folder > test_login_page + test_main_page

---
+++ section4/tests/test_login_page.py
@@ -0,0 +1,22 @@
+from Stepik.Автоматизация_тестирования_с_помощью_Selenium_и_Python.section4.pages.login_page import LoginPage
+
+
+def test_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_login_url()  # выполняем метод страницы — проверяем url страницы
+
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
