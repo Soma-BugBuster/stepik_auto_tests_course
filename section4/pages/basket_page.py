10ead7e380def6fb77129a6ef6d8a7ce8bcf5617
Soma <elena.valieva.QA@gmail.com>
2024-02-26 00:06:59 +0400

    Section 4 > lesson 3 > step 10
    ADD basket_page.py

---
+++ section4/pages/basket_page.py
@@ -0,0 +1,14 @@
+from Stepik.Автоматизация_тестирования_с_помощью_Selenium_и_Python.section4.pages.locators import BasketPageLocators
+from .base_page import BasePage
+
+
+class BasketPage(BasePage):
+    def should_be_basket_page(self):
+        self.should_be_empty_basket()
+
+    def should_be_empty_basket(self):
+        actual = self.check_element_text(*BasketPageLocators.BASKET_EMPTY_TEXT)
+        expected = "Your basket is empty. Continue shopping"
+        self.compare_elements_text(expected, actual)
+        assert self.is_not_element_present(*BasketPageLocators.ITEM_IN_BASKET), \
+            "Item(s) in the Basket are presented, but should not be"
