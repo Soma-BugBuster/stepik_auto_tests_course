1ba38c097c7efe2d63bdad8668a1c7c7a7b0bcd5
Soma <elena.valieva.QA@gmail.com>
2024-02-26 00:03:24 +0400

    Section 4 > lesson 3 > step 10
    ADD test_guest_cant_see_product_in_basket_opened_from_main_page

--- section4/tests/test_main_page.py
+++ section4/tests/test_main_page.py
@@ -1,3 +1,4 @@
+from Stepik.Автоматизация_тестирования_с_помощью_Selenium_и_Python.section4.pages.basket_page import BasketPage
 from Stepik.Автоматизация_тестирования_с_помощью_Selenium_и_Python.section4.pages.login_page import LoginPage
 from Stepik.Автоматизация_тестирования_с_помощью_Selenium_и_Python.section4.pages.main_page import MainPage
 
@@ -16,3 +17,12 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.open_basket()
+    basket = BasketPage(browser, MainPage.return_url)
+    basket.should_be_empty_basket()
