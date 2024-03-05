8cc7ac6fbf8ea048864e6d16a8e5c6ff1f243623
Soma <elena.valieva.QA@gmail.com>
2024-03-06 01:00:00 +0400

    Section 4 > lesson 3 > step 13
    CREATED

---
+++ section4/pages/variables.py
@@ -0,0 +1,6 @@
+class Links:
+    main_page = "http://selenium1py.pythonanywhere.com/"
+    product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    coders_promo_offer0 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207?promo=offer0"
+    city_and_the_stars = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
